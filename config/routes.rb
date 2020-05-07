7bcfe7810adfcbe42d79dee23102fa6a163bf170
Yuto-chan <63442439+Yuto-chan@users.noreply.github.com>
2020-05-07 14:26:56 +0900

    introducing haml n sass done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'transactions/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
+  resources :transactions, only: [:index]
 end
