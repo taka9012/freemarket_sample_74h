01f13287ee2d60ca87785f267a68af57e577fc96
fuminyan-238 <fuminyan238@gmail.com>
2020-05-09 18:12:27 +0900

    initial_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'items#index'
-  resources :items, only: [:index, :show]
+  root 'items#create'
+  resources :items, only: [:index, :show, :create]
 end
