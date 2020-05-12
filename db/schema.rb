ff2fa707ccbdddb489c8b2ed7668c24ee7fb7d8b
taka9012 <u.hide1201@icloud.com>
2020-05-12 13:11:15 +0900

    fix brands table
    nameカラムのNOT NULL削除

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2020_05_12_023113) do
   end
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
