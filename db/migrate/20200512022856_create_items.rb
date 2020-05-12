06767c4dbd3e8b5597bda2c7bcbba04614649ff2
taka9012 <u.hide1201@icloud.com>
2020-05-12 11:35:09 +0900

    create item model table
    ブランド、カテゴリー、アイテム、イメージのモデル、テーブル作成

---
+++ db/migrate/20200512022856_create_items.rb
@@ -0,0 +1,18 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string      :name           , null: false
+      t.text        :explanation    , null: false
+      t.integer     :price          , null: false
+      t.references  :brand          , foreign_key: true
+      t.references  :category       , null: false, foreign_key: true
+      t.string      :item_status    , null: false
+      t.string	    :postage_type   , null: false
+      t.string      :postage_burden , null: false
+      t.string      :shipping_area  , null: false
+      t.string      :shipping_date  , null: false
+      t.references  :user           , null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
