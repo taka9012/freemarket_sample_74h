dc79997fe3f7020f89e65e5a72dabccbf9ee828e
Mayumi <Kagura004@gmail.com>
2020-05-14 12:22:21 +0900

    add test-code of user's & addresse's models

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ group :development, :test do
   gem 'capistrano3-unicorn'
   gem 'capistrano-rails-console'
   gem 'rspec-rails'
+  gem 'factory_bot_rails'
 end
 
 group :development do
@@ -65,7 +66,8 @@ group :test do
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
-  gem 'chromedriver-helper'
+  # gem 'chromedriver-helper'
+  gem 'webdrivers'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
 
+  validates :firstname, :lastname, :firstname_kana, :lastname_kana, :postal_code, :prefectures, :city, :house_number, presence: true
+
   validates :firstname     , format: {with: /\A[ぁ-んァ-ン一-龥]/}
   validates :lastname      , format: {with: /\A[ぁ-んァ-ン一-龥]/}
   validates :firstname_kana, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :lastname_kana , format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :telephone_number, format: {with: /\A[0-9]{3}[0-9]{4}[0-9]{4}\z/}, length: { in: 10..11 }
+  validates :postal_code   , format: {with: /\A\d{7}\z/}
+  validates :telephone_number, format: {with: /\A\d{10,11}\z/}, unless: -> { telephone_number.blank? }
 end
