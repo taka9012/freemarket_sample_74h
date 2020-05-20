78e8e64187e61256898111c570c6dbd1a20f81bc
Mayumi <Kagura004@gmail.com>
2020-05-20 22:18:30 +0900

    fix show-item-detail display

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_090259) do
+ActiveRecord::Schema.define(version: 2020_05_20_124205) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "firstname", null: false
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_090259) do
     t.string "item_status_id", null: false
     t.string "postage_type_id", null: false
     t.string "postage_burden_id", null: false
-    t.string "shipping_area_id", null: false
+    t.string "shipping_area", null: false
     t.string "shipping_date_id", null: false
     t.string "trading_status_id", default: "1", null: false
     t.bigint "user_id", null: false
