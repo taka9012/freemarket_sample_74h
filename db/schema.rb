d081544acca37fe7033e047ff8cf59b7253380ff
Mayumi <Kagura004@gmail.com>
2020-05-21 15:14:54 +0900

    add index & null: true to ancestry migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_124205) do
+ActiveRecord::Schema.define(version: 2020_05_21_023037) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "firstname", null: false
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_124205) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "ancestry", null: false
+    t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["ancestry"], name: "index_categories_on_ancestry"
